96b9155cefddb0bf7da7c6fce76fb632f9f750bd
Manuel F <s253182@dtu.dk>
2026-03-20 17:20:43 +0100

    Completing the loadnetwork Zbus calculations and completing calculate_sequence_fault_currents, Calculate_sequence_fault_voltages, convert_sequence2pahse_currents and convert_sequence2phase_voltages methods in faultanalysis file

--- Assignment2/FaultAnalysis_46705.py
+++ Assignment2/FaultAnalysis_46705.py
@@ -29,29 +29,49 @@ def Calculate_Sequence_Fault_Currents(Zbus0,Zbus1,Zbus2,bus_to_ind,fault_bus,fau
     fb = bus_to_ind[fault_bus]
     if fault_type == 0:
         ''' Insert your code'''
+        Iseq[0] = Iseq[2] = 0
+        Iseq[1] = Vf/Zbus1[fb,fb]
     elif fault_type == 1:
         ''' Insert your code'''
+        Iseq[0] = Iseq[1] = Iseq[2] = Vf/(Zbus0[fb,fb] + Zbus1[fb,fb] + Zbus2[fb,fb] + 3*Zf)
     elif fault_type == 2:
         ''' Insert your code'''
+        Iseq[0] = 0
+        Iseq[1] = Vf/(Zbus1[fb,fb] + Zbus2[fb, fb] + Zf)
+        Iseq[2] = -Iseq[1]
     elif fault_type == 3:
         ''' Insert your code'''
+        Zeq = Zbus2[fb,fb]*(Zbus0[fb,fb] + 3*Zf)/(Zbus2[fb,fb] + Zbus0[fb,fb] + 3*Zf)
+        Iseq[1] = Vf/(Zbus1[fb,fb] + Zeq)
+        Iseq[2] = -Iseq[1]*(Zbus0[fb,fb] + 3*Zf)/(Zbus0[fb,fb] + Zbus2[fb,fb] + 3*Zf)
+        Iseq[0] = -Iseq[1]*Zbus2[fb,fb]/(Zbus0[fb,fb] + Zbus2[fb,fb] + 3*Zf)
     else:
         print('Unknown Fault Type')
     return Iseq
 
 # 1.2 the Calculate_Sequence_Fault_Voltages() function
 def Calculate_Sequence_Fault_Voltages(Zbus0,Zbus1,Zbus2,bus_to_ind,fault_bus,Vf,Iseq):
-    ''' Insert your code'''
+    fb = bus_to_ind[fault_bus]
+    V1 = Vf - Zbus1[fb,fb]*Iseq[1]
+    V2 = -Zbus2[fb,fb]*Iseq[2]
+    V0 = -Zbus0[fb,fb]*Iseq[0]
+    Vseq_mat = np.array([V0, V1, V2], dtype = complex)
     return Vseq_mat
 
 # 1.3. the Convert_Sequence2Phase_Currents() function
 def Convert_Sequence2Phase_Currents(Iseq):
-    ''' Insert your code'''
+    theta = np.deg2rad(120)
+    a = np.exp(1j*theta)
+    T = np.array([[1,1,1], [1, a**2, a], [1, a, a**2]])
+    Iph = T @ Iseq
     return Iph
 
 # 1.4 the Convert_Sequence2Phase_Voltages() function
 def Convert_Sequence2Phase_Voltages(Vseq_mat):
-    ''' Insert your code'''
+    theta = np.deg2rad(120)
+    a = np.exp(1j*theta)
+    T = np.array([[1,1,1], [1, a**2, a], [1, a, a**2]])
+    Vph_mat = T @ Vseq_mat
     return Vph_mat
 
 # ####################################################
