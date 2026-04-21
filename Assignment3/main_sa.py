80f783c6983a98182eca3268801d6f041dc9b394
matlafENSTA <mlafit@hotmail.fr>
2026-04-21 12:10:12 +0200

    A.4: apply_contingency_to_Y_matrices

--- Assignment3/main_sa.py
+++ Assignment3/main_sa.py
@@ -54,7 +54,7 @@ def System_violations(V,Ybus,Y_from,Y_to,lnd):
 
 def apply_contingency_to_Y_matrices(Ybus,Yfr,Yto,fr_ind,to_ind,br_ind,Ybr_mat):
     # input:
-    # The original admittance matirces: Ybus,Yfr,Yto
+    # The original admittance matrices: Ybus,Yfr,Yto
     # The from and to end indices for the branch (fr_ind, to_ind)
     # The indice for where the branch is in the branch list (br_ind)
     # The 2x2 admittance matrix for the branch Ybr_mat
@@ -64,9 +64,14 @@ def apply_contingency_to_Y_matrices(Ybus,Yfr,Yto,fr_ind,to_ind,br_ind,Ybr_mat):
     Yfr_mod = Yfr . copy ( ) # when ever you make changes to Ybus_mod
     Yto_mod = Yto . copy ( ) # using the .copy() function avoids this
     ##################################################################################
-    # YOUR CODE COMES HERE:
     # 1. Remove the branch from the Ybus_mod matrix
+    Ybus_mod[fr_ind,fr_ind] -= Ybr_mat[0,0]
+    Ybus_mod[to_ind,to_ind] -= Ybr_mat[1,1]
+    Ybus_mod[fr_ind,to_ind] -= Ybr_mat[0,1]
+    Ybus_mod[to_ind,fr_ind] -= Ybr_mat[1,0]
     # 2. Remove the branch from the Yto and Yfr matrices
+    Yfr_mod[br_ind,:] = 0
+    Yto_mod[br_ind,:] = 0
     ##################################################################################
     return Ybus_mod,Yfr_mod,Yto_mod
 
